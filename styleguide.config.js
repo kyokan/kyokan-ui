5357607ff526e886a005a3fc748d65c1dce5251a
Ravi S. Rāmphal <rramphal@gmail.com>
2018-07-24 07:25:05 -0700

    update component path

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,8 +2,17 @@ const path = require('path');
 
 module.exports = {
   title: 'Kyokan UI Style Guide',
+
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/styleguide/ThemeWrapper'),
     LogoRenderer: path.join(__dirname, 'src/styleguide/ThemeToggle'),
+  },
+
+  components: 'src/components/**/[A-Z]*.jsx',
+
+  getComponentPathLine (componentPath) {
+    const name = path.basename(componentPath, '.jsx')
+    const dir = path.dirname(componentPath)
+    return `import ${name} from '${dir}';`
   },
 };
