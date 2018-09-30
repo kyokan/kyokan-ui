52104bcafc0e8968710a2caa7bfff6c0499866ae
Ravi S. Rāmphal <rramphal@gmail.com>
2018-09-30 00:56:59 -0700

    adjusted styleguide behavior

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  title: 'Kyokan UI Style Guide',
+  components: 'src/components/**/[A-Z]*.jsx',
+
+  /* show code example by default */
+  exampleMode: 'expand',
+
+  /* show each component on its own page */
+  pagePerSection: true,
 
   styleguideComponents: {
-    Wrapper: path.join(__dirname, 'src/styleguide/ThemeWrapper'),
-    LogoRenderer: path.join(__dirname, 'src/styleguide/ThemeToggle'),
+    Wrapper      : path.join(__dirname, 'src/styleguide/ThemeWrapper'),
+    LogoRenderer : path.join(__dirname, 'src/styleguide/ThemeToggle'),
   },
-
-  components: 'src/components/**/[A-Z]*.jsx',
 
   template: {
     head: {
@@ -21,8 +25,13 @@ module.exports = {
     },
   },
 
+  title: 'Kyokan UI Style Guide',
+
+  /* show props and methods table by default */
+  usageMode: 'expand',
+
   getComponentPathLine (componentPath) {
     const name = path.basename(componentPath, '.jsx')
-    return `import ${name} from 'kyokan-ui';`
+    return `import { ${name} } from 'kyokan-ui';`
   },
 };
