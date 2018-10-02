c9cb6ed1ce305d00b9c7a95cd748e1c1813d3828
Ravi S. Rāmphal <rramphal@gmail.com>
2018-10-02 05:01:05 -0700

    colors linting

--- src/themes/DMLTheme/sections/_colors.js
+++ src/themes/DMLTheme/sections/_colors.js
@@ -9,20 +9,20 @@ function darken (color, value) {
 }
 
 const color01 = 'rgb( 60, 129, 237)'; // #3C81ED Royal Blue
-const color02 = 'rgb(177, 204, 248)'; // #B1CCF8 Sail
+// const color02 = 'rgb(177, 204, 248)'; // #B1CCF8 Sail
 const color03 = 'rgb( 13,  41,  87)'; // #0D2957 Downriver
 const color04 = 'rgb(108, 128, 160)'; // #6C80A0 Steelplant
 const color05 = 'rgb(129, 148, 178)'; // #8194B2 Bali Hai
 const color06 = 'rgb(173, 188, 212)'; // #ADBCD4 Casper
-const color07 = 'rgb(218, 225, 237)'; // #DAE1ED Muriel Light
+// const color07 = 'rgb(218, 225, 237)'; // #DAE1ED Muriel Light
 const color08 = 'rgb(225, 232, 242)'; // #E1E8F2 Catskill White
-const color09 = 'rgb(247, 249, 252)'; // #F7F9FC Cloud White
+// const color09 = 'rgb(247, 249, 252)'; // #F7F9FC Cloud White
 const color10 = 'rgb( 41,  54,  74)'; // #29364A Pickled Bluewood
-const color11 = 'rgb(237, 240, 245)'; // #EDF0F5 Mystic
-const color12 = 'rgb(255, 200,   0)'; // #FFC800 Supernova
+// const color11 = 'rgb(237, 240, 245)'; // #EDF0F5 Mystic
+// const color12 = 'rgb(255, 200,   0)'; // #FFC800 Supernova
 
 const white   = 'rgb(255, 255, 255)'; // #FFFFFF White
-const black   = 'rgb(  0,   0,   0)'; // #000000 Black
+// const black   = 'rgb(  0,   0,   0)'; // #000000 Black
 
 const primaryColorHover  = lighten(color01, 0.2);
 const primaryColorActive = lighten(color01, 0.1);
