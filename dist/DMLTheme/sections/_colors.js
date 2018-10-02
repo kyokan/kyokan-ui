de98e3ea0a784dec14c23072dfe8f7149329347e
Ravi S. Rāmphal <rramphal@gmail.com>
2018-10-02 01:53:03 -0700

    removed unused colors

--- dist/DMLTheme/sections/_colors.js
+++ dist/DMLTheme/sections/_colors.js
@@ -20,10 +20,7 @@ const color09 = 'rgb(247, 249, 252)'; // #F7F9FC Cloud White
 const color10 = 'rgb( 41,  54,  74)'; // #29364A Pickled Bluewood
 const color11 = 'rgb(237, 240, 245)'; // #EDF0F5 Mystic
 const color12 = 'rgb(255, 200,   0)'; // #FFC800 Supernova
-const color15 = 'rgba(  0,   0,   0, .5)'; // #000000 Black
-const color16 = 'rgb(248, 102, 102)'; // #000000 Carnation Red
-const color17 = 'rgba( 60, 129, 237, .95)'; // #3C81ED Royal Blue
-const color18 = 'rgba( 60, 129, 237, .9)'; // #3C81ED Royal Blue
+
 const white = 'rgb(255, 255, 255)'; // #FFFFFF White
 const black = 'rgb(  0,   0,   0)'; // #000000 Black
 
