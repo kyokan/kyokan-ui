aa9f7e30dba482acaa17a863516e294edf5950a7
Richard Webber <xydlebug@gmail.com>
2018-10-02 22:34:33 +0100

    Add overlay support to HorizontalRule.

--- dist/HorizontalRule/HorizontalRule.js
+++ dist/HorizontalRule/HorizontalRule.js
@@ -1,16 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 /**
-* A horizontal rule.
+* A horizontal rule with optional text overlay.
 */
 const HorizontalRule = props => {
-  const StyledHorizontalRule = styled.hr`
-    color:        ${props => props.theme.horizontalRuleColor};
-    border-style: ${props => props.theme.horizontalRuleBorderStyle}
+
+  const StyledContainer = styled.div`
+    position        : relative;
+    display         : flex;
+    justify-content : center;
   `;
 
-  return React.createElement(StyledHorizontalRule, null);
+  const StyledText = styled.div`
+    color            : ${({ theme }) => theme.panelTextColor};
+    background-color : ${({ theme }) => theme.panelBackgroundColor};
+    padding          : 0 0.5rem;
+    z-index          : 100;
+  `;
+
+  const StyledHorizontalRule = styled.hr`
+    color        : ${({ theme }) => theme.horizontalRuleColor};
+    border-style : ${({ theme }) => theme.horizontalRuleBorderStyle};
+    position     : absolute;
+    top          : 0;
+    left         : 0;
+    width        : 100%;
+  `;
+
+  return React.createElement(
+    StyledContainer,
+    { className: props.className },
+    React.createElement(StyledHorizontalRule, null),
+    props.text && React.createElement(
+      StyledText,
+      null,
+      props.text
+    )
+  );
+};
+
+HorizontalRule.propTypes = {
+  /** Class name for top-level wrapper **/
+  className: PropTypes.string,
+
+  /** Overlay text */
+  text: PropTypes.string
+};
+
+HorizontalRule.defaultProps = {
+  className: ''
 };
 
 /** @component */
--- src/components/HorizontalRule/HorizontalRule.jsx
+++ src/components/HorizontalRule/HorizontalRule.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 /**
-* A horizontal rule.
+* A horizontal rule with optional text overlay.
 */
 const HorizontalRule = (props) => {
+
+  const StyledContainer = styled.div`
+    position        : relative;
+    display         : flex;
+    justify-content : center;
+  `;
+
+  const StyledText = styled.div`
+    color            : ${ ({ theme }) => theme.panelTextColor };
+    background-color : ${ ({ theme }) => theme.panelBackgroundColor };
+    padding          : 0 0.5rem;
+    z-index          : 100;
+  `;
+
   const StyledHorizontalRule = styled.hr`
-    color:        ${ (props) => props.theme.horizontalRuleColor };
-    border-style: ${ (props) => props.theme.horizontalRuleBorderStyle }
+    color        : ${ ({ theme }) => theme.horizontalRuleColor };
+    border-style : ${ ({ theme }) => theme.horizontalRuleBorderStyle };
+    position     : absolute;
+    top          : 0;
+    left         : 0;
+    width        : 100%;
   `;
 
   return (
-    <StyledHorizontalRule />
+    <StyledContainer className={ props.className }>
+      <StyledHorizontalRule />
+      { props.text && <StyledText>{ props.text }</StyledText> }
+    </StyledContainer>
   );
+};
+
+HorizontalRule.propTypes = {
+  /** Class name for top-level wrapper **/
+  className: PropTypes.string,
+
+  /** Overlay text */
+  text: PropTypes.string,
+};
+
+HorizontalRule.defaultProps = {
+  className: '',
 };
 
 /** @component */
